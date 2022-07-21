fd0dd9c2389d4d64237b1c9bfb5e078b159f14e5
jin <tjrdn110@naver.com>
2022-07-21 20:55:22 +0900

    JPAmapping

--- src/main/java/com/shop/demo/entity/Item.java
+++ src/main/java/com/shop/demo/entity/Item.java
@@ -5,10 +5,9 @@ import com.shop.demo.constant.ItemSellStatus;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.stereotype.Controller;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
@@ -19,16 +18,24 @@ import java.time.LocalDateTime;
 public class Item {
 
     @Id
+    @Column(name = "item_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(nullable = false, length = 50)
     private String itemNm;
 
+    @Column(name = "price", nullable = false)
     private int price;
 
+    @Column(nullable = false)
     private int stockNumber;
 
+    @Lob
+    @Column(nullable = false)
     private String itemDetail;
 
+    @Enumerated(EnumType.STRING)
     private ItemSellStatus itemSellStatus;
 
     private LocalDateTime regTime;
